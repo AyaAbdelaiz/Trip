e8ac472175276c4fd56bb0dc810954da82aecb11
doaaabdelnaser <96724380+doaaabdelnaser@users.noreply.github.com>
2022-01-25 12:58:25 +0200

    This is Completed UI and Linked between som activity

---
+++ app/src/main/java/com/app/trip/notes_from_widgt.java
@@ -0,0 +1,20 @@
+package com.app.trip;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class notes_from_widgt extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes_from_widgt, container, false);
+    }
+}
