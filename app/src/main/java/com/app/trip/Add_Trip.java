1625a3c42f8e99db589e7c6cfb452bd8e966586f
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2022-01-25 04:28:35 +0200

    nnn

---
+++ app/src/main/java/com/app/trip/Add_Trip.java
@@ -0,0 +1,14 @@
+package com.app.trip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Trip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+}
